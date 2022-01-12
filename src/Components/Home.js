d1bb9495733a63a6ff4c78cf95b9b07049fa4279
Asonglefac Millenium <asonglefacmillenium@gmail.com>
2022-01-12 22:08:28 +0100

    added dark section

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from './Header/Header.js'
 import "./Home.js"
+import Homebody from './Homebody/Homebody.js'
 
 const Home = () => {
     return (
         <div className="home">
         <Header/>
+        <Homebody/>
             
         </div>
     )
---
+++ src/Components/Homebody/Homebody.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./Homebody.css"
+
+const Homebody = () => {
+    return (
+        <div className="homebody">
+            <div className="dark-background">
+                <img src="../assets/darkback.png" alt="" />
+                <div className="dark-content">
+                    <h3>Not sure where to go? Perfect</h3>
+                    <button>I am flexible</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Homebody
