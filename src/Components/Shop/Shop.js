8b1c26c030ba5740c23a8c096152535e82ae5ae8
Asonglefac Millenium <asonglefacmillenium@gmail.com>
2022-01-21 23:16:01 +0100

    shop gift

--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -5,6 +5,11 @@ const Shop = () => {
     return (
         <div className="shop">
         <ShopHead/>
+
+        <div className="shop-head">
+            <h1>Airbnb <br /> gift cards</h1>
+            <img src="../assets/" alt="" />
+        </div>
             
         </div>
     )
